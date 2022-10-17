d8f143d119821f030fc0da5fd1bc9465117e696c
mcgue <mcgu@protonmail.com>
2022-10-17 10:50:42 -0700

    get user name

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+name = input('Let\'s play. Please enter your name: ')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(name)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
