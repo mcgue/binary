7106aeef210ac4c14d16b9203fd26dfee85e2a04
mcgue <mcgu@protonmail.com>
2022-10-20 11:07:16 -0700

    add comments, code to get guess

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Binary Search Algorithm
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Get player name
 name = input('Let\'s play. Please enter your name: ')
+
+# Get number guess
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
