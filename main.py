e19b09bfaf1b1e1026ed1b97e2d373497449f3f6
mcgue <mcgu@protonmail.com>
2022-10-25 17:37:18 -0700

    add comments

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ name = input('I will try to guess your number. Please enter your name: ')
 number = input('Please enter your guess: ')
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    # function to greet player
+    print(f'Hi, {name}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
