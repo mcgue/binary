a0fa398a1aa842843515c5ccbe404d7e2bd7b2ff
mcgue <mcgu@protonmail.com>
2022-10-28 13:12:12 -0700

    start counter function

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def print_hi(name):
     # function to greet player
     print(f'Hi, {name}')
 
+# Counter function
+def counter():
+    print(number)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi(name)
+    counter()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
