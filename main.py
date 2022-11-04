42b525b372a890d7bb830863b0b938ce38fdfd6c
mcgue <mcgu@protonmail.com>
2022-11-03 20:16:17 -0700

    more writing searcher function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Binary Search Algorithm
 
 # Get player name
-name = input('I will try to guess your number. Please enter your name: ')
+player = input('I will try to guess your number. Please enter your name: ')
 
 def print_hi(name):
     # function to greet player
@@ -10,12 +10,15 @@ def print_hi(name):
 # Counter function
 def searcher():
     # Get number guess
-    number = input('Please enter your guess: ')
-    print(number)
+    number = int(input('Please enter a number between 1 and 100: '))
+    if number < 50:
+        print('Your number is less than 50')
+    else:
+        print('Greater than or equal to 50')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi(name)
-    counter()
+    print_hi(player)
+    searcher()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
