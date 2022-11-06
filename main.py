6a578d02f2973f068745efef12cb8db7095913b6
mcgue <mcgu@protonmail.com>
2022-11-06 09:41:20 -0800

    more writing searcher function

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def print_hi(name):
 def searcher():
     # Get number guess
     response = input('Is your number less than 50? Y or N ')
-    if response[0].upper() == 'Y':
-        print('Your number is less than 50')
-    elif response[0].upper() == 'N':
+    response = response[0].upper()
+    while response != 'Y' and response != 'N':
+        response = input('Is your number less than 50? Y or N ')
+        response = response[0].upper()
+    if response == 'Y':
+        response = input('Is it less than 25? Y or N ')
+    elif response == 'N':
         print('Greater than or equal to 50')
-    else:
-        print('Please enter Y or N')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
