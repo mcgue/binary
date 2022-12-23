fa27ad90960ce8e289fcb205eac01ef66cf0dda5
mcgue <mcgu@protonmail.com>
2022-12-23 09:07:03 -0800

    correct recursive search to get it to work

--- main.py
+++ main.py
@@ -3,41 +3,34 @@
 import secrets
 
 def rec_search(arr, low, high, x):
-    # Calculate half point
-    print(str(low) + ' ' + str(high))
+    # Check low < high and if already matches
     if high >= low:
-        mid = low + (high - low) // 2
-
+        mid = (low + high)//2
         # Check if already matches
         if arr[mid] == x:
             return mid
         # If middle larger, then do search on lower half
         elif arr[mid] > x:
-            return rec_search(arr, low, mid, x)
+            return rec_search(arr, low, high-1, x)
         # If middle lower, then do search on upper half
         else:
-            return rec_search(arr, mid+1, high, x)
-    # X not present
+            return rec_search(arr, low+1, high, x)
     else:
         return -1
 
 # Run if main
 if __name__ == '__main__':
-    # Get range to search
-    print('What is the range to search? Please enter an integer.')
-    while True:
-        try:
-            bot = int(input('Lowest number? '))
-        except ValueError:
-            print("Please enter a valid integer")
-            continue
-        else:
-            print(f'You entered the low for the range: {bot}')
-            break
-    top = int(input('Highest number? '))
-    list_search = list(range(bot, top+1, 1))
+    # Generate sorted list
+    list_search = list(range(0, 11, 2))
     print(list_search)
+    # Set low and high
+    low = 0
+    high = len(list_search)-1
     # Generate number to search
-    x = int(secrets.choice(list_search))
+    x = int(secrets.randbelow(11))
     print(x)
-    print(str(x) + ' found at index ' + str(rec_search(list_search, bot, top, x)))
+    result = rec_search(list_search, low, high, x)
+    if result == -1:
+        print('Not found')
+    else:
+        print(str(x) + ' found at index ' + str(result))
