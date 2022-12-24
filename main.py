3975774dfab6297f4206ec3594d86b9ac586ee87
mcgue <mcgu@protonmail.com>
2022-12-24 15:14:15 -0800

    finish comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# Binary Search Algorithm
-# import secrets for random number
+# Recursive Binary Search Algorithm
+
+# import secrets module for random number
 import secrets
 
 def rec_search(arr, low, high, x):
-    # Check low < high and if already matches
+    # Check low < high
     if high >= low:
         mid = (low + high)//2
         # Check if already matches
@@ -15,22 +16,26 @@ def rec_search(arr, low, high, x):
         # If middle lower, then do search on upper half
         else:
             return rec_search(arr, low+1, high, x)
+    # return -1 if not found
     else:
         return -1
 
 # Run if main
 if __name__ == '__main__':
+    # Set top of range
+    top = 1000
     # Generate sorted list
-    list_search = list(range(0, 11, 2))
+    list_search = list(range(0, top, 3))
     print(list_search)
-    # Set low and high
+    # Set low and high for indices
     low = 0
     high = len(list_search)-1
     # Generate number to search
-    x = int(secrets.randbelow(11))
-    print(x)
+    x = int(secrets.randbelow(top))
+    # Run function to search list
     result = rec_search(list_search, low, high, x)
+    # print results of running search
     if result == -1:
-        print('Not found')
+        print(str(x) + ' not found in list.')
     else:
         print(str(x) + ' found at index ' + str(result))
